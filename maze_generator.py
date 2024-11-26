c656f9f09a898febb7297be0697eed51f1802721
Gourav Bhardwaj <gouravbhardwaj3007@gmail.com>
2024-11-26 15:53:26 -0800

    Verifying if the A* search is working fine, comapring it with Backtracker from API, did not work

--- maze_generator.py
+++ maze_generator.py
@@ -1,5 +1,31 @@
 # Using mazelib API to generate a maze for our inputs
 # Source : https://github.com/john-science/mazelib
 from mazelib import Maze
-from mazelib.generate.Prims import Prims
+from mazelib.solve.BacktrackingSolver import BacktrackingSolver
+from mazelib.generate.DungeonRooms import DungeonRooms
 
+
+
+import numpy as np
+
+if __name__ == '__main__':
+    m = Maze()
+    m.generator = DungeonRooms(20, 20)
+    m.generate()
+
+    # print("Maze")
+    # print(m.tostring())
+
+    m.solver = BacktrackingSolver()
+    m.start = (0, 1)
+    m.end = (17, 15)
+    # print("Maze")
+    # print(m.tostring(True))
+    
+    m.solve()
+
+    print("Solution")
+    print(m.tostring(True, True))
+    
+    maze = np.array(m.grid, dtype=np.uint8)
+    maze.tofile('maze.bin')     
