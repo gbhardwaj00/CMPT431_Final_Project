Corrected the calculation for rows and cols in the maze_reader file

maze_generator.py
@@ -1,10 +1,8 @@
 # Using mazelib API to generate a maze for our inputs
 # Source : https://github.com/john-science/mazelib
 from mazelib import Maze
-from mazelib.solve.BacktrackingSolver import BacktrackingSolver
+from mazelib.solve.RandomMouse import RandomMouse
 from mazelib.generate.DungeonRooms import DungeonRooms
-
-
 
 import numpy as np
 
@@ -16,16 +14,17 @@ if __name__ == '__main__':
     # print("Maze")
     # print(m.tostring())
 
-    m.solver = BacktrackingSolver()
+    m.solver = RandomMouse()
     m.start = (0, 1)
     m.end = (17, 15)
     # print("Maze")
     # print(m.tostring(True))
     
     m.solve()
-
+    
     print("Solution")
     print(m.tostring(True, True))
     
     maze = np.array(m.grid, dtype=np.uint8)
-    maze.tofile('maze.bin')     
+    print(maze)
+    maze.tofile('maze.bin')